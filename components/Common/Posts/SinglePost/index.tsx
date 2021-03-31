af286b38cf08fb8fe46ee213040507d73c5bbedc
iMykhailychenko <igor.c.m@ukr.net>
2021-03-31 21:03:17 +0300

    ...

---
+++ components/Common/Posts/SinglePost/index.tsx
@@ -0,0 +1,98 @@
+import { faKeyboard, faTimesCircle } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { ReactElement } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import config from '../../../../assets/config';
+import { formatDate } from '../../../../assets/helpers';
+import routes from '../../../../assets/routes';
+import useAuth from '../../../../hooks/auth.hook';
+import { IPost, IState, IUser } from '../../../../interfaces';
+import types from '../../../../redux/types';
+import Likes from '../../Likes';
+import ProfileBig from '../../Profile/ProfileBig';
+import css from '../index.module.css';
+
+const DESC_LIMIT = 180;
+
+interface IProps {
+    item: IPost;
+    author?: boolean;
+}
+
+const SinglePost = ({ item, author }: IProps): ReactElement => {
+    const auth = useAuth();
+    const { query } = useRouter();
+    const dispatch = useDispatch();
+
+    const profile = useSelector<IState, IUser | null>(state => state.profile);
+
+    const handleDelete = (): void => {
+        dispatch({
+            type: types.DELETE_POST_START,
+            config: { page: query?.page || 1, limit: config.postPerPage },
+            payload: item._id,
+        });
+    };
+
+    return (
+        <li className={clsx(css.card, !item.banner && css.grid)} key={item?._id}>
+            <Link href={routes.posts.single[0](item?._id)}>
+                <a className={css.postLink}>
+                    {item.banner && <img className={css.img} src={config.img(item.banner)} alt={item.title} />}
+                    <div className={css.inner}>
+                        <h4 className={css.title}>{item.title}</h4>
+                        <p className={css.text}>
+                            {item?.desc?.length > DESC_LIMIT ? `${item?.desc?.slice(0, DESC_LIMIT)}...` : item?.desc}
+                        </p>
+                    </div>
+                </a>
+            </Link>
+            {item.tags?.length ? (
+                <div className={css.tags}>
+                    {item.tags.map(tag => (
+                        <Link href={routes.posts.tag[0](tag)} key={tag}>
+                            <a className={css.tag}>{`#${tag}`}</a>
+                        </Link>
+                    ))}
+                </div>
+            ) : null}
+            <p className={css.date}>{formatDate(item.date)}</p>
+
+            {auth?.token && (author ? profile?._id : item?.author?.[0]?._id) === auth?.user?._id && (
+                <div className={css.manage}>
+                    <Link href={routes.posts.edit[0](item._id)}>
+                        <a>
+                            <FontAwesomeIcon icon={faKeyboard} />
+                            <span>Edit post</span>
+                        </a>
+                    </Link>
+                    <button type="button" onClick={handleDelete}>
+                        <FontAwesomeIcon icon={faTimesCircle} />
+                        <span>Delete post</span>
+                    </button>
+                </div>
+            )}
+
+            <div className={css.inner}>
+                <ProfileBig user={author ? profile : item?.author?.[0]} />
+            </div>
+
+            <div className={css.likes}>
+                <Likes
+                    targetId={item?._id}
+                    feedback={item.feedback}
+                    queue={item.queue}
+                    typeLike={types.LIKE_POPULAR_POSTS_START}
+                    typeDislike={types.DISLIKE_POPULAR_POSTS_START}
+                    typeQueue={types.UPDATE_QUEUE_POPULAR_START}
+                />
+            </div>
+        </li>
+    );
+};
+
+export default SinglePost;
--- components/Common/Posts/index.tsx
+++ components/Common/Posts/index.tsx
@@ -1,28 +1,9 @@
-import { faKeyboard, faTimesCircle } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import clsx from 'clsx';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React, { ReactElement } from 'react';
 import Masonry from 'react-masonry-css';
-import { useDispatch, useSelector } from 'react-redux';
 
-import config from '../../../assets/config';
-import { formatDate } from '../../../assets/helpers';
-import routes from '../../../assets/routes';
-import useAuth from '../../../hooks/auth.hook';
-import { IPost, IState, IUser } from '../../../interfaces';
-import types from '../../../redux/types';
-import Likes from '../Likes';
-import ProfileBig from '../Profile/ProfileBig';
+import { IPost } from '../../../interfaces';
 import css from './index.module.css';
-
-interface IProps {
-    content: IPost[];
-    wide?: boolean;
-    author?: boolean;
-    col?: number;
-}
+import SinglePost from './SinglePost';
 
 type Media = { [key: number]: number; default: number };
 
@@ -33,85 +14,22 @@ const media = (col: number): Media => ({
     610: 1,
 });
 
-const DESC_LIMIT = 180;
+interface IProps {
+    content: IPost[];
+    wide?: boolean;
+    author?: boolean;
+    col?: number;
+    isColumn?: boolean;
+}
 
-const Posts = ({ content, col = 2, author }: IProps): ReactElement => {
-    const dispatch = useDispatch();
-    const auth = useAuth();
-    const { query } = useRouter();
-
-    const profile = useSelector<IState, IUser | null>(state => state.profile);
-
-    const handleDelete = (payload: string): (() => void) => (): void => {
-        dispatch({
-            type: types.DELETE_POST_START,
-            config: { page: query?.page || 1, limit: config.postPerPage },
-            payload,
-        });
-    };
-
-    return (
+const Posts = ({ content, col = 2, author, isColumn = false }: IProps): ReactElement => {
+    return isColumn ? (
+        <ul className={css.column}>
+            {content?.length && content?.map(item => <SinglePost key={item._id} item={item} author={author} />)}
+        </ul>
+    ) : (
         <Masonry breakpointCols={media(col)} className={css.list} columnClassName={css.column}>
-            {content?.length &&
-                content?.map(item => (
-                    <li className={clsx(css.card, !item.banner && css.grid)} key={item?._id}>
-                        <Link href={routes.posts.single[0](item?._id)}>
-                            <a className={css.postLink}>
-                                {item.banner && (
-                                    <img className={css.img} src={config.img(item.banner)} alt={item.title} />
-                                )}
-                                <div className={css.inner}>
-                                    <h4 className={css.title}>{item.title}</h4>
-                                    <p className={css.text}>
-                                        {item?.desc?.length > DESC_LIMIT
-                                            ? `${item?.desc?.slice(0, DESC_LIMIT)}...`
-                                            : item?.desc}
-                                    </p>
-                                </div>
-                            </a>
-                        </Link>
-                        {item.tags?.length ? (
-                            <div className={css.tags}>
-                                {item.tags.map(tag => (
-                                    <Link href={routes.posts.tag[0](tag)} key={tag}>
-                                        <a className={css.tag}>{`#${tag}`}</a>
-                                    </Link>
-                                ))}
-                            </div>
-                        ) : null}
-                        <p className={css.date}>{formatDate(item.date)}</p>
-
-                        {auth?.token && (author ? profile?._id : item?.author?.[0]?._id) === auth?.user?._id && (
-                            <div className={css.manage}>
-                                <Link href={routes.posts.edit[0](item._id)}>
-                                    <a>
-                                        <FontAwesomeIcon icon={faKeyboard} />
-                                        <span>Edit post</span>
-                                    </a>
-                                </Link>
-                                <button type="button" onClick={handleDelete(item._id)}>
-                                    <FontAwesomeIcon icon={faTimesCircle} />
-                                    <span>Delete post</span>
-                                </button>
-                            </div>
-                        )}
-
-                        <div className={css.inner}>
-                            <ProfileBig user={author ? profile : item?.author?.[0]} />
-                        </div>
-
-                        <div className={css.likes}>
-                            <Likes
-                                targetId={item?._id}
-                                feedback={item.feedback}
-                                queue={item.queue}
-                                typeLike={types.LIKE_POPULAR_POSTS_START}
-                                typeDislike={types.DISLIKE_POPULAR_POSTS_START}
-                                typeQueue={types.UPDATE_QUEUE_POPULAR_START}
-                            />
-                        </div>
-                    </li>
-                ))}
+            {content?.length && content?.map(item => <SinglePost key={item._id} item={item} author={author} />)}
         </Masonry>
     );
 };
